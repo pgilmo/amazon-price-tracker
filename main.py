a73c848b80c1da2be5601ae1402adc782d2be503
pgilmo <pablogilmochon2006@gmail.com>
2026-05-03 22:41:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from bs4 import BeautifulSoup
 import time
 from datetime import datetime
 
-url = 'https://www.amazon.es/UBUNTU-25-10-Memoria-Bootable-Linux/dp/B0FZBN1B1H/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&sr=8-1'
-desired_price = 10
+# 1. Paste the full Amazon URL of the product you want to track
+url = '' 
+# 2. Set the target price you are waiting for (in Euros)
+desired_price = 0
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
 }
